d314fb477b3013b1c3cc3e2d627d7d8b570e3446
Seb Boulard <boulard.seb@gmail.com>
2019-02-20 18:15:13 +0100

    add empty service in plugin example

---
+++ lib/svc.EmptyPlugin.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *	minim - PHP framework
+    Copyright (C) 2019  Sébastien Boulard
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; see the file COPYING. If not, write to the
+    Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+final class EmptyPlugin extends CoreCommon {
+	
+	private $oContactMgr = NULL;
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function getDashboard() {
+		/*
+		$sDashboard = Dashboard::getDashboard('{__CONTACT_MGR_HOME_TITLE__}', $sContents);
+		Allez voir la classe Dashboard ;)
+		 */
+		return '';
+	}
+	
+	public function ceQueVousVoulez() {
+		/*
+		Vous ouvez récupérer tout ce que vous voulez de la requête émise ici,
+		via la classe UserRequest,
+		à vous de jouer et de renvoyer le résultat attendu.
+		 * 
+		 * Toutes les fonctions doivent être public.
+		 * C'est une instance qui est utilisée.
+		 * 
+		 * Enjoy
+		 * 
+		*/
+	} 
+}
